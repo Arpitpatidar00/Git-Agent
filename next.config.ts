6d737d67bdc746f0fcb81bf8294c61888b19db16
Arpit Patidar <arpitpatidar@Arpits-MacBook-Air.local>
2026-07-15 11:56:32 +0530

    fix:comfig origin

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // Allow hot module reloading over ngrok
-  allowedDevOrigins: [
-    "2efd-2409-40c4-281-44bd-309b-7b3c-4f22-d89.ngrok-free.app",
-  ],
+  allowedDevOrigins: ["git-agent-delta.vercel.app/"],
 };
 
 export default nextConfig;
